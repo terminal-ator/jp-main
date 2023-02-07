fc5494a177dfe2bed47d8691b5308770956f8928
Tushar Garg <itushargarg@gmail.com>
2023-02-07 16:59:53 +0530

    moving to better pc

---
+++ src/lib/directus.ts
@@ -0,0 +1,3 @@
+import { Directus } from '@directus/sdk';
+
+
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,11 +1,23 @@
 <section>
-
-<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    
-    <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">FMCG products delivered right.</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Get your products placed in over 600 stores in the dadri-greater noida region. With quick deliveries and high efficiency, we are unlike other distributors. </p>
+    <div
+        class="flex flex-col mx-auto items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-gray-800"
+    >
+        <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5
+                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+                FMCG products delivered right.
+            </h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Get your products placed in over 600 stores in the dadri-greater
+                noida region. With quick deliveries and high efficiency, we are
+                unlike other distributors.
+            </p>
+            <a
+                href="mailto:ttc_dadri@yahoo.com"
+                class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                >Email us</a
+            >
+        </div>
     </div>
-</a>
-
 </section>
