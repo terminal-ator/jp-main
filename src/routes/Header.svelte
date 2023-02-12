8286e6e32bf798137ec03efec2b00a9f4a70828e
Tushar <itushargarg@gmail.com>
2023-02-12 08:14:37 +0530

    improved the design further

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,7 @@
 <header>
-  <nav class="bg-white border-gray-200 px-4 sm:px-6 py-2.5 dark:bg-gray-800">
+  <nav
+    class=" fixed top-0 left-0 w-full border-gray-200 px-4 sm:px-6 py-2.5 dark:bg-gray-800"
+  >
     <div
       class="flex flex-wrap  justify-center items-center mx-auto w-full sm:max-w-screen-xl"
     >
